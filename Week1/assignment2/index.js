4429a63303ef3e66da301c0fe5386cfa4af2c39f
IamAltay <etliekmek97@gmail.com>
2024-05-03 16:53:17 +0200

    update 2

--- Week1/assignment2/index.js
+++ Week1/assignment2/index.js
@@ -20,5 +20,22 @@ connection.query(
     });
   }
 );
+console.log('-------------------------------------');
+connection.query(
+  'SELECT name FROM country WHERE name LIKE "%land%" ',
+  function (error, results) {
+    if (error) throw error;
 
+    results.forEach((element) => {
+      console.log(element.name);
+    });
+  }
+);
+console.log('-------------------------------------');
+
+//SELECT name FROM city WHERE population > 500000 AND population < 1000000;
+
+//SELECT name FROM country WHERE continent = "Europe";
+
+// 5)
 connection.end();
